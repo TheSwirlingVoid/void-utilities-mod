e9a214cf2e14a66877929ebb0ece00afe0a44bbe
TheSwirlingVoid <59374649+TheSwirlingVoid@users.noreply.github.com>
2020-01-03 09:22:30 -0800

    yee

--- src/main/java/org/theswirlingvoid/VoidUtilities/Main.java
+++ src/main/java/org/theswirlingvoid/VoidUtilities/Main.java
@@ -9,7 +9,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("voidutilities")
--- src/main/java/org/theswirlingvoid/VoidUtilities/ServerProxy.java
+++ src/main/java/org/theswirlingvoid/VoidUtilities/ServerProxy.java
@@ -2,7 +2,6 @@ package org.theswirlingvoid.VoidUtilities;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.DistExecutor;
 
 public class ServerProxy implements IProxy {
 	
