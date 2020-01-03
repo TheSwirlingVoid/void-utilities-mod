6e3bcb40ce711fa65cc84c9f3bc5a29d0a29fc01
Epicmoofish <40126677+Epicmoofish@users.noreply.github.com>
2020-01-03 09:16:44 -0800

    removed gay warnings

--- src/main/java/org/theswirlingvoid/VoidUtilities/ServerProxy.java
+++ src/main/java/org/theswirlingvoid/VoidUtilities/ServerProxy.java
@@ -1,8 +1,6 @@
 package org.theswirlingvoid.VoidUtilities;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.DistExecutor;
 
 public class ServerProxy implements IProxy {
 	
