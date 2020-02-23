206ed2e14714df47ff81306aad701add3716d3db
Epicmoofish <40126677+Epicmoofish@users.noreply.github.com>
2020-02-23 10:17:52 -0800

    added textures to effects and beaconnt

--- src/main/java/org/theswirlingvoid/VoidUtilities/effects/EffectHandler.java
+++ src/main/java/org/theswirlingvoid/VoidUtilities/effects/EffectHandler.java
@@ -12,7 +12,7 @@ import net.minecraftforge.registries.ObjectHolder;
 public class EffectHandler {
 	public static final CustomEffect light = (CustomEffect) new CustomEffect(EffectType.HARMFUL, 255,255,255).setRegistryName("light").addAttributesModifier(SharedMonsterAttributes.KNOCKBACK_RESISTANCE, "7407DE5E-7CE8-4030-940E-514C1F160890", -1, AttributeModifier.Operation.ADDITION);
 	public static final CustomEffect fragile = (CustomEffect) new CustomEffect(EffectType.HARMFUL, 255,255,255).setRegistryName("fragile");
-	public static final MyopiaEffect myopia = (MyopiaEffect) new MyopiaEffect(EffectType.HARMFUL, 0,0,0).setRegistryName("myopia").addAttributesModifier(SharedMonsterAttributes.FOLLOW_RANGE, "7407DE5E-7CE8-4030-940E-514C1F160890", -0.5, AttributeModifier.Operation.MULTIPLY_BASE);
+	public static final MyopiaEffect myopia = (MyopiaEffect) new MyopiaEffect(EffectType.HARMFUL, 0,0,0).setRegistryName("myopia").addAttributesModifier(SharedMonsterAttributes.FOLLOW_RANGE, "7408DE5E-7CE8-4030-940E-514C1F160890", -0.5, AttributeModifier.Operation.MULTIPLY_BASE);
 //	public static final CustomEffect fragile = Main.Null();
 //	public static final CustomEffect myopia = Main.Null();
 }
